66088d3ecfa67cd189201b0253f9072b680fe105
Ivan Hribernik <cihribernik@gmail.com>
2025-06-20 07:49:30 -0300

    feat: added base django rdf app

---
+++ services/backend/backend/urls.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path, re_path, reverse_lazy
+from django.views.generic.base import RedirectView
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from .products.views import OrderViewSet, ProductViewSet
+from .users.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet)
+router.register(r"products", ProductViewSet)
+router.register(r"orders", OrderViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/", include(router.urls)),
+    path("api-token-auth/", views.obtain_auth_token),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    re_path(r"^$", RedirectView.as_view(url=reverse_lazy("api-root"), permanent=False)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ services/backend/wait_for_mysql.py
@@ -0,0 +1,36 @@
+import os
+import logging
+from time import time, sleep
+import MySQLdb
+
+check_timeout = int(os.getenv("MYSQL_CHECK_TIMEOUT", 30))
+check_interval = int(os.getenv("MYSQL_CHECK_INTERVAL", 1))
+interval_unit = "second" if check_interval == 1 else "seconds"
+config = {
+    "db": os.getenv("MYSQL_DATABASE", "app_db"),
+    "user": os.getenv("MYSQL_USER", "root"),
+    "passwd": os.getenv("MYSQL_PASSWORD", "root"),
+    "host": os.getenv("MYSQL_HOST", "mysql"),
+    "port": int(os.getenv("MYSQL_PORT", 3306)),
+}
+
+start_time = time()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+def wait_for_mysql(db, user, passwd, host, port):
+    while time() - start_time < check_timeout:
+        try:
+            conn = MySQLdb.connect(db=db, user=user, passwd=passwd, host=host, port=port)
+            logger.info("MySQL is ready! ✨ 💅")
+            conn.close()
+            return True
+        except Exception:
+            logger.info(f"MySQL isn't ready. Waiting for {check_interval} {interval_unit}...")
+            sleep(check_interval)
+
+    logger.error(f"We could not connect to MySQL within {check_timeout} seconds.")
+    return False
+
+wait_for_mysql(**config)
